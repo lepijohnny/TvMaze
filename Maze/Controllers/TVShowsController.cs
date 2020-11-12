facf00d3d99c448745adc0aaaa15b2382c8e4b90
lepijohnny <lepijohnny@gmail.com>
2020-11-12 16:49:00 +0100

    Inject IUnitOfWork per request instead of in ctor

--- Maze/Controllers/TVShowsController.cs
+++ Maze/Controllers/TVShowsController.cs
@@ -9,69 +9,46 @@ namespace Maze.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TVShowsController : ControllerBase, IDisposable
-    {
-        private IUnitOfWork _unitOfWork;
+    public class TVShowsController : ControllerBase
+    {        
         private readonly ILogger<TVShowsController> _logger;
 
-        private bool _disposed = false;
-
-        public TVShowsController(IUnitOfWork unitOfWork, ILogger<TVShowsController> logger)
+        public TVShowsController(ILogger<TVShowsController> logger)
         {
-            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
         // GET api/tvshows
         [HttpGet]
-        public async Task<ActionResult<string>> Get(int page)
+        public async Task<ActionResult<string>> Get([FromServices]IUnitOfWork unitOfWork, int page)
         {
-            string apiMethod = $"api/tvshows?page={page}";
-
-            _logger.LogInformation($"{apiMethod} started ...");
-
-            var cmd = new GetPaginatedTVShowsCommand(_unitOfWork, Math.Max(0, page));
-
-            try
+            using (unitOfWork)
             {
-                var result = await cmd.Execute();
-   
-                var (payload, last, totalCount) = result.Value;
+                string apiMethod = $"api/tvshows?page={page}";
 
-                Response.Headers.Add("X-Pagination-Last", last.ToString());
-                Response.Headers.Add("X-Pagination-Total", totalCount.ToString());
+                _logger.LogInformation($"{apiMethod} started ...");
 
-                _logger.LogInformation($"{apiMethod} finised ...");
+                var cmd = new GetPaginatedTVShowsCommand(unitOfWork, Math.Max(0, page));
 
-                return Ok(payload);
+                try
+                {
+                    var result = await cmd.Execute();
+
+                    var (payload, last, totalCount) = result.Value;
+
+                    Response.Headers.Add("X-Pagination-Last", last.ToString());
+                    Response.Headers.Add("X-Pagination-Total", totalCount.ToString());
+
+                    _logger.LogInformation($"{apiMethod} finised ...");
+
+                    return Ok(payload);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"{apiMethod} thrown an exception: {ex} ...");
+                    return StatusCode(500);
+                }
             }
-            catch(Exception ex)
-            {
-                _logger.LogError($"{apiMethod} thrown an exception: {ex} ...");
-                return StatusCode(500);
-            }
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (_disposed)
-            {
-                return;
-            }
-
-            if (disposing)
-            {
-                _unitOfWork?.Dispose();
-                _unitOfWork = null;
-            }
-
-            _disposed = true;
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
         }
     }
 }
